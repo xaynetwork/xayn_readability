30d4efb075bcfcae58d78ca76e0abe0683e9c887
DESKTOP-68EULOT\Igindo-Frank <frank@igindo.com>
2021-11-05 13:18:24 +0100

    expose makeReadable

--- lib/xayn_readability.dart
+++ lib/xayn_readability.dart
@@ -5,5 +5,6 @@ export 'package:flutter_widget_from_html_core/flutter_widget_from_html_core.dart
 export 'src/controllers/reader_mode_controller.dart';
 export 'src/process_html/objects/parser_options.dart';
 export 'src/process_html/process_html.dart';
+export 'src/widgets/jobs/make_readable.dart';
 export 'src/widgets/objects/process_html_result.dart';
 export 'src/widgets/reader_mode.dart';
